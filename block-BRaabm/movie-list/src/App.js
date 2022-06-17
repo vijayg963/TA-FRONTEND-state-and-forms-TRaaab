e2820bbf3dce3eb5f5a0927ff6c417006d54036d
Vijay Gupta <vijayg963@gmail.com>
2022-06-17 23:31:55 +0530

    Movie-list-App

---
+++ block-BRaabm/movie-list/src/App.js
@@ -0,0 +1,13 @@
+import Header from './Header';
+import Main from './Main';
+
+function App() {
+  return (
+    <div className='app'>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
